dd1c7638c4c5072695b2d8cd9c291612f985e80f
Richard Baker <richb@coj.net>
2012-01-30 09:38:36 -0500

    Added additional comments to the config section to explain each line-item

--- app/view/Tab1.js
+++ app/view/Tab1.js
@@ -11,10 +11,23 @@ Ext.define('App.view.Tab1', {
     alias   : 'widget.tab1',
     title   : 'Tab One',
     config: {
+        //In this case, this title is never seen.
         title       : 'Tab One',
+        
+        //Determines what icon is used on the tab button
+        //Default icons: (http://docs.sencha.com/touch/2-0/source/Button.html)
+        //  action, add, arrow_down, arrow_left, arrow_right, arrow_up, compose, delete,
+        //  organize, refresh, reply, search, settings, star, trash, maps, locate, home
         iconCls     : 'star',
+        
+        //CSS class that will be added in the div wrapper
+        //@docs http://docs.sencha.com/touch/2-0/#!/api/Ext.Component-cfg-cls
         cls         : 'tab1',
+        
+        //Defaults to false; no scroll
         scrollable  : 'auto',
+        
+        //Here you can pack in all the items you want to be in this view
         items : [
             {
                 xtype   : 'titlebar',
