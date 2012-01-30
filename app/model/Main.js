76504d1ca7afd0a1e7134912fe06971d043db01d
Richard Baker <richb@coj.net>
2012-01-30 08:50:09 -0500

    Basic model

---
+++ app/model/Main.js
@@ -0,0 +1,14 @@
+/**
+ * @class App.model.Main
+ * @extends Ext.data.Model
+ */
+Ext.define('App.model.Main', {
+    extend: 'Ext.data.Model',
+
+    config: {
+        fields: [
+            {name: "id",    type: "int"}
+        ]
+    },
+    
+});
